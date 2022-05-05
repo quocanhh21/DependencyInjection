282a03d09bf7c7e408cf5ca137d7cea477e8f397
anh.le <anh.le@PC-ANHLE>
2022-05-05 15:56:33 +0700

    Add project files.

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    public class Employee
+    {
+        public void Work()
+        {
+            Console.WriteLine("Employee Working...");
+        }
+    }// class employee is a dependency of company
+  
+    public class Company
+    {
+        private Employee _myDependency;
+        private string _emailMessage;
+        //by constructor
+        public Company(Employee myDependency)
+        {
+            this._myDependency = myDependency;
+        }
+
+        //by setter
+        public void SendMail(string message) 
+        {
+            this._emailMessage= message;
+        }
+        public void GetEmail()
+        {
+            Console.WriteLine($"{_emailMessage}");
+        }
+        public void ActionB()
+        {
+            _myDependency.Work();
+        }
+    }// company depend on employee
+
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+          
+            Company obj2= new Company(new Employee());
+
+            obj2.SendMail("----demo mail from setter----");
+            obj2.ActionB();
+            obj2.GetEmail();
+           
+        }
+    }
+}
